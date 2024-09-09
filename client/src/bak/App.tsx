c26eaf21f83db25eec452e0ad03cfcded53f6f24
Careteen <15074806497@163.com>
2024-09-09 16:52:13 +0800

    feat: 🎸 增加 URL.createObjectURL 使用

--- client/src/bak/App.tsx
+++ client/src/bak/App.tsx
@@ -2,40 +2,25 @@ import { transform } from "@babel/standalone";
 import { useRef } from "react";
 
 function App() {
-  const addCode = `
-function add(a: number, b: number) {
-  return a + b
-}
-export { add }
-`;
+  const code = `import { useState } from "react";
 
-  const callAddCode = `
-import { add } from './add';
-console.log(add(1, 2));  
+function App() {
+  const [num, setNum] = useState(0);
+  return <div onClick={() => setNum((prev) => prev + 1)}>{num}</div>;
+}
+
+export default App;
 `;
 
   const ref = useRef<HTMLTextAreaElement>(null);
-
-  const addCodeUrl = URL.createObjectURL(
-    new Blob([addCode], { type: "application/javascript" })
-  );
 
   function onClick() {
     if (!ref.current) {
       return;
     }
-    const res = transform(callAddCode, {
+    const res = transform(ref.current.value, {
       presets: ["react", "typescript"],
       filename: "App.tsx",
-      plugins: [
-        {
-          visitor: {
-            ImportDeclaration(path) {
-              path.node.source.value = addCodeUrl;
-            },
-          },
-        },
-      ],
     });
     console.log(res.code);
   }
@@ -45,7 +30,7 @@ console.log(add(1, 2));
       <textarea
         ref={ref}
         style={{ width: "500px", height: "300px" }}
-        defaultValue={callAddCode}
+        defaultValue={code}
       ></textarea>
       <button onClick={onClick}>编译</button>
     </div>
