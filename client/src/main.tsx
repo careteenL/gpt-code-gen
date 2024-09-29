8cec9f45574b9c8c62e0b6f2de6c0e98d9d7ca1d
Careteen <15074806497@163.com>
2024-09-29 23:10:44 +0800

    feat: 🎸 add sentry

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,20 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://65573a49b04ead2470440f349645714c@o4508035572498432.ingest.us.sentry.io/4508036778754048",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
